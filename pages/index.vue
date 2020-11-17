9e036ae858f9fd1c89450d753c2663b300c6366d
Hiroki Kawaguchi <kawakanu@gmail.com>
2020-11-17 15:38:18 +0900

    fix: card image width

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@
           <div v-for="(v, k) in images" :key="k" class="col-md-4">
             <div class="card mb-4 shadow-sm">
               <!-- <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg> -->
-              <div class="trim">
+              <div class="card-img-top trim">
                 <img
                   :src="v.src"
                   :height="v.height"
