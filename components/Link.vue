ca9085aa4606ae3f5dff7f0cd5a56121a7fd353a
Hiroki Kawaguchi <kawakanu@gmail.com>
2020-11-22 02:30:13 +0900

    add: link component

---
+++ components/Link.vue
@@ -0,0 +1,44 @@
+<template>
+  <p>
+    <nuxt-link
+      to="/"
+      class="btn my-2"
+      :class="{ 'btn-primary': path === '/', 'btn-secondary': path !== '/' }"
+    >
+      No lazy loading
+    </nuxt-link>
+    <nuxt-link
+      to="/observer"
+      class="btn my-2"
+      :class="{
+        'btn-primary': path === '/observer',
+        'btn-secondary': path !== '/observer',
+      }"
+    >
+      Intersection Observer
+    </nuxt-link>
+    <nuxt-link
+      to="/image"
+      class="btn my-2"
+      :class="{
+        'btn-primary': path === '/image',
+        'btn-secondary': path !== '/image',
+      }"
+    >
+      @nuxt/image
+    </nuxt-link>
+  </p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  props: {
+    path: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
