d89c034669a1f98f15ffc7790534de8436659bd5
Hiroki Kawaguchi <kawakanu@gmail.com>
2020-11-21 18:54:53 +0900

    add: @nuxt/image

---
+++ pages/module.vue
@@ -0,0 +1,63 @@
+<template>
+  <main role="main">
+    <section class="jumbotron text-center">
+      <div class="container">
+        <h1>test-nuxt-intersection-observer</h1>
+        <p class="lead text-muted">@nuxt/image</p>
+      </div>
+    </section>
+    <div class="album py-5 bg-light">
+      <div class="container">
+        <div class="row">
+          <div v-for="(v, k) in images" :key="k" class="col-md-4 mx-md-auto">
+            <div class="card mb-4 shadow-sm">
+              <!-- <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg> -->
+              <NuxtImage
+                class="card-img-top"
+                :src="v.src"
+                :alt="v.alt"
+              />
+              <div class="card-body">
+                <p class="card-text">
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button
+                      type="button"
+                      class="btn btn-sm btn-outline-secondary"
+                    >
+                      View
+                    </button>
+                    <button
+                      type="button"
+                      class="btn btn-sm btn-outline-secondary"
+                    >
+                      Edit
+                    </button>
+                  </div>
+                  <small class="text-muted">9 mins</small>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import images from '~/imageList.js'
+
+export default defineComponent({
+  setup() {
+    return {
+      images,
+    }
+  },
+})
+</script>
