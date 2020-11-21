59a755d9b2aa19908d1f4a4f94e22347c2616616
Hiroki Kawaguchi <kawakanu@gmail.com>
2020-11-22 02:35:44 +0900

    fix: change card image size

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <section class="jumbotron text-center">
       <div class="container">
         <h1>test-nuxt-intersection-observer</h1>
-        <p class="lead text-muted">lazy = false</p>
+        <Link :path="path" />
       </div>
     </section>
     <div class="album py-5 bg-light">
@@ -12,38 +12,14 @@
           <div v-for="(v, k) in images" :key="k" class="col-md-4">
             <div class="card mb-4 shadow-sm">
               <!-- <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg> -->
-              <div class="card-img-top trim">
-                <img
-                  :src="v.src"
-                  :height="v.height"
-                  :width="v.width"
-                  :alt="v.alt"
-                />
-              </div>
-              <div class="card-body">
-                <p class="card-text">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button
-                      type="button"
-                      class="btn btn-sm btn-outline-secondary"
-                    >
-                      View
-                    </button>
-                    <button
-                      type="button"
-                      class="btn btn-sm btn-outline-secondary"
-                    >
-                      Edit
-                    </button>
-                  </div>
-                  <small class="text-muted">9 mins</small>
-                </div>
-              </div>
+              <img
+                :alt="v.alt"
+                :src="v.src"
+                class="card-img-top"
+                height="225"
+                width="348"
+              />
+              <CardBody />
             </div>
           </div>
         </div>
@@ -53,13 +29,16 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, useContext } from '@nuxtjs/composition-api'
 import images from '~/imageList.js'
 
 export default defineComponent({
   setup() {
+    const { route } = useContext()
+
     return {
       images,
+      path: route.value.path,
     }
   },
 })
