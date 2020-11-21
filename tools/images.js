69ad3ba56a14cf8791689735dc185796856b7304
Hiroki Kawaguchi <kawakanu@gmail.com>
2020-11-22 02:25:19 +0900

    fix: image list order

--- tools/images.js
+++ tools/images.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const imageSize = require('image-size')
 
-class ImageReader {
+class FsWrapper {
   constructor() {
     this.charCode = 'utf-8'
   }
@@ -16,41 +16,57 @@ class ImageReader {
   }
 }
 
+class ListSort {
+  compare(a, b) {
+    const re = new RegExp('^lighthouse-(\\d+?)_(\\d+?)\\.(\\w+?)$')
+
+    const result1 = re.exec(a)
+    const result2 = re.exec(b)
+
+    const id1 = parseInt(result1[1])
+    const id2 = parseInt(result2[1])
+
+    if (id1 < id2) return -1
+    if (id1 > id2) return 1
+    return 0
+  }
+}
+
 function main() {
   const cwd = process.cwd()
+  console.log('cwd', cwd)
+
   const READ_DIR = path.join(cwd, '../static/images/sample/')
   const OUTPUT_FILE = path.join(cwd, '../imageList.js')
   const MAX_IMAGE_COUNT = 30
 
-  console.log('cwd', cwd)
+  const fw = new FsWrapper()
+  const imageNameList = fw.readDir(READ_DIR)
 
-  const ir = new ImageReader()
-  const images = ir.readDir(READ_DIR)
+  const ls = new ListSort()
+  imageNameList.sort(ls.compare)
 
   const output = []
+  let i = 0
 
-  images.forEach((i) => {
-    if (output.length === MAX_IMAGE_COUNT) {
-      return
-    }
+  while (i < MAX_IMAGE_COUNT) {
+    const imageName = imageNameList[i]
 
-    const imagePath = path.join(READ_DIR, i)
-    console.info(imagePath)
-
+    const imagePath = path.join(READ_DIR, imageName)
     const dimensions = imageSize(imagePath)
-    console.info(dimensions)
 
     output.push({
-      src: '/images/sample/' + i,
+      src: '/images/sample/' + imageName,
       height: dimensions.height.toString(),
       width: dimensions.width.toString(),
-      alt: i,
+      alt: imageName,
     })
-  })
+
+    i++
+  }
 
   const data = 'export default ' + JSON.stringify(output)
-
-  ir.writeFile(OUTPUT_FILE, data)
+  fw.writeFile(OUTPUT_FILE, data)
 }
 
 main()
